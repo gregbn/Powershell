237e184bf49ddb84aac782ebe0992f940014bd1f
gregbn <grzegorzburnos@gmail.com>
2019-02-05 22:49:12 +0100

    Update disable_dvddrive.ps1
    Added if statement - check if instance id is not null

--- disable_dvddrive.ps1
+++ disable_dvddrive.ps1
@@ -1,2 +1,11 @@
-$drive =  (Get-PnpDevice | Select Friendlyname, Class, instanceid |  where {$_.Class -like "CDROM"}).instanceid
-Disable-PnpDevice -InstanceId $drive -Confirm:$false
+# Disabling optical drive in Windows 10
+
+# search for optical drive in PnP devices and get it's instance id
+$driveInstance =  (Get-PnpDevice | Select Friendlyname, Class, InstanceId |  where {$_.Class -like "CDROM"}).InstanceId
+
+# if InstanceId exists, disable the device
+	if ($driveInstance) {
+  
+		# disable optical drive using it's instance id
+		Disable-PnpDevice -InstanceId $driveInstance -Confirm:$false
+  }
